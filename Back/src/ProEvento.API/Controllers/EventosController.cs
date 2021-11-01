581a227b8515111a8217633619b52f3445f81eba
taina <taina-correa195@hotmail.com>
2021-11-01 11:39:47 -0300

    segundo commit

---
+++ Back/src/ProEvento.API/Controllers/EventosController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEvento.API.Data;
+using ProEvento.API.Models;
+
+namespace ProEvento.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+
+       private readonly DataContext _context;
+        public EventosController(DataContext context)
+        {
+           _context = context;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        
+        {
+           return _context.Eventos;
+        }
+
+        [HttpPost]
+
+        public string Post()
+        {
+           return "Estamos no metodo Post ;-)";
+        }
+
+        [HttpDelete("{id}")]
+
+        public string Delete(int id)
+        {
+           return $"Agora Delete com o id={id}";
+        }
+
+    }
+}
--- Back/src/ProEvento.API/Startup.cs
+++ Back/src/ProEvento.API/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ProEvento.API.Data;
 
 namespace ProEvento.API
 {
@@ -26,8 +28,10 @@ namespace ProEvento.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(
+                context =>context.UseSqlite(Configuration.GetConnectionString("default")));
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEvento.API", Version = "v1" });
@@ -49,6 +53,12 @@ namespace ProEvento.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(
+                x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin()
+            );
 
             app.UseEndpoints(endpoints =>
             {
